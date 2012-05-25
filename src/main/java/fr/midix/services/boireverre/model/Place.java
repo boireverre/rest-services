d9c57c684aa387176a5df055cb1db08ef7dcca03
dirix <michel.dirix@inria.fr>
2012-05-25 15:45:43 +0200

    adding geocoder

--- src/main/java/fr/midix/services/boireverre/model/Place.java
+++ src/main/java/fr/midix/services/boireverre/model/Place.java
@@ -1,10 +1,18 @@
 package fr.midix.services.boireverre.model;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
+import com.google.code.geocoder.model.GeocoderResult;
 
 /**
  *
@@ -13,8 +21,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Place")
 public class Place implements Serializable{
-    
-    @Id
+  
+	private static final long serialVersionUID = 1L;
+	@Id
     @GeneratedValue
     private long id;
     private String name;
@@ -28,6 +37,14 @@ public class Place implements Serializable{
     public Place(String name, String address){
     	this.name = name;
     	this.address = address;
+    	final Geocoder geocoder = new Geocoder();
+    	GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(address+", France").setLanguage("en").getGeocoderRequest();
+    	GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+    	List<GeocoderResult> results = geocoderResponse.getResults();
+    	if(results.size()>0){
+    		this.latitude = results.get(0).getGeometry().getLocation().getLat().floatValue();
+    		this.longitude = results.get(0).getGeometry().getLocation().getLng().floatValue();
+    	}
     }
     
     public long getId() {
@@ -37,6 +54,38 @@ public class Place implements Serializable{
     public void setId(long id) {
         this.id = id;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
     
     
 }
